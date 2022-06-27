ff51f2b0d61490bc71422a40756e3c65db13dce3
Your Name <you@example.com>
2022-06-27 17:12:28 +0300

    https://github.com/my-great-day/todo.git

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -1,10 +1,6 @@
-from flask import Blueprint, request, jsonify
-from flask_jwt_extended import create_access_token, current_user, jwt_required
-
-from .. import db
+from flask import Blueprint
 
 todo = Blueprint('todo', __name__, template_folder='templates', static_folder='static')
 api = Blueprint('api', __name__)
 
 from . import views
-from ..models import Users, Content
